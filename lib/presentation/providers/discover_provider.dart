c5cd3bfa0f855d7e24e8c188fdc1348cc3882187
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-07-12 01:19:10 -0300

    md add videos to discover provider

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:tik_tok_clone/domain/entities/video_post.dart';
+import 'package:tik_tok_clone/infrastructure/models/local_video_model.dart';
+import 'package:tik_tok_clone/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  //TODO: Repository, DataSource
+
   bool initialLoading = true;
 
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // todo: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
 
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList(); // for convert from Iterable<VideoPost> to List<VideoPost>
+
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
