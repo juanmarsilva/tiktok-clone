32b34a06612374badf61c0ca7ef5a5b6775245e3
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-07-21 00:22:30 -0300

    pause videos

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -43,18 +43,27 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
               ),
             );
           }
-          return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(
-              children: [
-                VideoPlayer(controller),
-                Positioned(
-                    bottom: 50,
-                    left: 20,
-                    child: _VideoCaption(
-                      caption: widget.caption,
-                    )),
-              ],
+          return GestureDetector(
+            onTap: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+                return;
+              }
+              controller.play();
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  VideoPlayer(controller),
+                  Positioned(
+                      bottom: 50,
+                      left: 20,
+                      child: _VideoCaption(
+                        caption: widget.caption,
+                      )),
+                ],
+              ),
             ),
           );
         });
